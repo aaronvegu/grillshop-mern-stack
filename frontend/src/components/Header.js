63652fd81a9fbdc30424c307f1d4375e56e774e3
Aaron Vegu <aaron@reckoner-rde.io>
2020-10-21 23:49:54 -0500

    React Router implemented for routing the product page from Home and Logo, cart and login pages from the header. Using Link from react-router-dom and LinkContainer from react-router-bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,16 +7,22 @@ const Header = () => {
     <header>
       <Navbar className='navbar-dark bg-primary' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='#home'>Grill Shop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Grill Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i> Ingresar
-              </Nav.Link>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i> Carrito
-              </Nav.Link>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Ingresar
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Carrito
+                </Nav.Link>
+              </LinkContainer>
               {/* <NavDropdown title='Categorias' id='basic-nav-dropdown'>
               <NavDropdown.Item href='#action/3.1'>
                 Categoria 1
