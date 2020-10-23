bfa7f1bd39271428a6231ac90defb94fc1d4390b
Aaron Vegu <aaron@reckoner-rde.io>
2020-10-23 18:59:08 -0500

    Configure HomeScreen to get the products from the backend server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  // El uso de states es declarado dentro de nuestra React Function debido a que como es una funcion
+  // y no una clase, no cuenta con constructor
+
+  // Recibe: [nombreDelEstado, nombreFuncionParaManipularElEstado]
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      // Desestructuring: it could be-> const res = await... and use res.data
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Cortes Premium</h1>
