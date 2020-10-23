5f5445ba8aa44fc34fc0f974625ca278b9cf2cff
Aaron Vegu <aaron@reckoner-rde.io>
2020-10-23 17:57:07 -0500

    ProductScreen Screen implemented on a basic way only to show the product data, it does not display the stock of any type of product, that is something to be implemented

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
 
-const ProductScreen = () => {
-  return <div>Product Page</div>;
+const ProductScreen = (props) => {
+  const product = products.find((p) => p._id === props.match.params.id);
+
+  return (
+    <>
+      <Link className='btn btn-dark my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup varian='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Precio: ${product.type[0].price}</ListGroup.Item>
+            <ListGroup.Item>
+              Descripción: <br /> {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Precio:</Col>
+                  <Col>
+                    <strong>${product.type[0].price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.type[0].typeStock > 0
+                      ? 'En Existencia'
+                      : 'Agotado'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.type[0].typeStock === 0}
+                >
+                  Agregar al carrito
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
