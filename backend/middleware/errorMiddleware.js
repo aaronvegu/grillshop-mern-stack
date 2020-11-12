d1a38593cede5aab9020a8a5f8d63f1c413f617b
Aaron Vegu <aaron@reckoner-rde.io>
2020-11-12 10:29:10 -0600

    Middleware to handle custom errors and 404 not found error added to server.js

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,23 @@
+// Middleware to handle custom errors
+
+// 404 Not Found Error
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// Custom error handler
+const errorHandler = (err, req, res, next) => {
+  // sometime we get a 200 status code even with a error, so we can get a 500
+  // code when this happen
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
